50c15f643f3702142f3976fa713b2f911de2a577
Minhyeok Seo <smh2153@gmail.com>
2021-01-15 20:51:09 +0900

    new chart

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ chartLink = ["ExperienceContribution", "SiegeDamage", "HeroDamage", "TimeSpentDe
 chartTitle = ["EXP Contribution", "Siege Damage", "Hero Damage", "Time Spent Dead", "Damage Taken", "Teamfight Damage Taken", "Teamfight Damage Dealt", "Minion Kills"]
 chartLinkID = ["Exp", "SiegeDmg", "HeroDmg", "DeathTime", "DmgTaken", "TeamFightDmgTaken", "TeamFightDmg", "MinionKills"]
 
-global chatHistory, teamBlue, teamRed, player
-player = list(dict() for i in range(0, 10))
+global chatHistory, teamBlue, teamRed, players
+players = list(dict() for i in range(0, 10))
 version = '83004'
 Language = 'enus'
 with open('./json/herodata_{}_{}.json'.format(version, Language), encoding='utf-8') as json_file:
@@ -32,7 +32,7 @@ def looptime(t):
     return (t - 610) / 16
 
 def open_replay(replay_file):
-    for i in player:
+    for i in players:
         i['RegenGlobesTime'] = list()
         i['Pings'] = list()
         i['Spray'] = list()
@@ -69,15 +69,14 @@ def open_replay(replay_file):
     gameevents = archive.read_file('replay.game.events')
     messageevents = archive.read_file('replay.message.events')
     attributeevents = protocol.decode_replay_attributes_events(archive.read_file('replay.attributes.events'))
-    heroList = list()
-    nameList = list()
-    mapName = details['m_title'].decode()
-    gameVersion = "{}.{}.{}.{}".format(header['m_version']['m_major'], header['m_version']['m_minor'], header['m_version']['m_revision'], header['m_version']['m_baseBuild'])
-    gameTime = datetime.fromtimestamp((details['m_timeUTC'] + details['m_timeLocalOffset']) // 10000000 - 11644506000)
-    gameLength = '{0:02d}:{1:02d}'.format(int(header['m_elapsedGameLoops'] / 16) // 60, int((header['m_elapsedGameLoops'] / 16) % 60))
-    gameMode = "Unknown"
+    gameDetails = dict()
+    gameDetails['mapName'] = details['m_title'].decode()
+    gameDetails['gameVersion'] = "{}.{}.{}.{}".format(header['m_version']['m_major'], header['m_version']['m_minor'], header['m_version']['m_revision'], header['m_version']['m_baseBuild'])
+    gameDetails['gameTime'] = datetime.fromtimestamp((details['m_timeUTC'] + details['m_timeLocalOffset']) // 10000000 - 11644506000)
+    gameDetails['gameLength'] = '{0:02d}:{1:02d}'.format(int(header['m_elapsedGameLoops'] / 16) // 60, int((header['m_elapsedGameLoops'] / 16) % 60))
+    gameDetails['gameMode'] = "Unknown"
     if initdata['m_syncLobbyState']['m_gameDescription']['m_gameOptions']['m_ammId'] in gameModeDict:
-        gameMode = gameModeDict[initdata['m_syncLobbyState']['m_gameDescription']['m_gameOptions']['m_ammId']]
+        gameDetails['gameMode'] = gameModeDict[initdata['m_syncLobbyState']['m_gameDescription']['m_gameOptions']['m_ammId']]
 
     if hasattr(protocol, 'decode_replay_tracker_events'):
         contents = archive.read_file('replay.tracker.events')
@@ -100,7 +99,7 @@ def open_replay(replay_file):
                 if event['m_eventName'].decode() == 'EndOfGameTimeSpentDead':
                     time_spent_dead = event['m_fixedData'][0]['m_value'] / 4096
                     player_number = event['m_intData'][0]['m_value'] - 1
-                    player[player_number]['TimeSpentDead'] = time_spent_dead
+                    players[player_number]['TimeSpentDead'] = time_spent_dead
                 
                 #Camp Capture
                 if event['m_eventName'].decode() == 'JungleCampCapture':
@@ -142,20 +141,19 @@ def open_replay(replay_file):
                                     thisHero = Hero
                                     thisHeroName = herodata[Hero]["name"]
                                     break
-                            player[player_number][i['m_key'].decode()] = thisHero
-                            player[player_number]['heroName'] = thisHeroName
-                            heroList.append(thisHeroName)
-                            player[player_number]['heroLink'] = player[player_number]['Hero']
-                            player[player_number]['partyPanelPortrait'] = herodata[player[player_number]['heroLink']]['portraits']['partyPanel']
-                            player[player_number]['minimap'] = herodata[player[player_number]['heroLink']]['portraits']['minimap']
+                            players[player_number][i['m_key'].decode()] = thisHero
+                            players[player_number]['heroName'] = thisHeroName
+                            players[player_number]['heroLink'] = players[player_number]['Hero']
+                            players[player_number]['partyPanelPortrait'] = herodata[players[player_number]['heroLink']]['portraits']['partyPanel']
+                            players[player_number]['minimap'] = herodata[players[player_number]['heroLink']]['portraits']['minimap']
                             
                 #Talents
                         elif i['m_key'].decode() != 'Win/Loss' and i['m_key'] != 'Map':
-                            for which_talent in herodata[player[player_number]['Hero']]["talents"]["level{}".format(talent_tier[j])]:
+                            for which_talent in herodata[players[player_number]['Hero']]["talents"]["level{}".format(talent_tier[j])]:
                                 
                                 if which_talent["nameId"] == i['m_value'].decode():
-                                    player[player_number]['talentIcon'][j - 1] = which_talent['icon']
-                                    player[player_number]['talent'] += talent_sort[which_talent['sort'] - 1]
+                                    players[player_number]['talentIcon'][j - 1] = which_talent['icon']
+                                    players[player_number]['talent'] += talent_sort[which_talent['sort'] - 1]
                                     j += 1
                                     continue
                 
@@ -167,33 +165,33 @@ def open_replay(replay_file):
                 if event['m_eventName'].decode() == 'PlayerDeath':
                     time = looptime(event['_gameloop'])
                     dead_player_number = event['m_intData'][0]['m_value'] - 1
-                    player[dead_player_number]['Death'].append(time)
+                    players[dead_player_number]['Death'].append(time)
                     for i in range(1, len(event['m_intData'])):
                         player_number = event['m_intData'][i]['m_value'] - 1
-                        player[player_number]['PlayerKill'].append((time, dead_player_number))
+                        players[player_number]['PlayerKill'].append((time, dead_player_number))
                 
                 #Spray
                 if event['m_eventName'].decode() == 'LootSprayUsed':
                     time = looptime(event['_gameloop'])
                     player_number = event['m_intData'][0]['m_value'] - 1
-                    player[player_number]['Spray'].append(time)
+                    players[player_number]['Spray'].append(time)
 
                 #VoiceLine
                 if event['m_eventName'].decode() == 'LootVoiceLineUsed':
                     time = looptime(event['_gameloop'])
                     player_number = event['m_intData'][0]['m_value'] - 1
-                    player[player_number]['VoiceLine'].append(time)
+                    players[player_number]['VoiceLine'].append(time)
 
                 #Regen Globe Pickup
                 if event['m_eventName'].decode() == 'RegenGlobePickedUp':
                     time = looptime(event['_gameloop'])
                     player_number = event['m_intData'][0]['m_value'] - 1
-                    player[player_number]['RegenGlobesTime'].append(time)
+                    players[player_number]['RegenGlobesTime'].append(time)
                     
                 
 
 
-    for i in player:
+    for i in players:
         i['talent'] = i['talent'].ljust(14, '0')
     # Hero name, Player name, Team
     player_number = 0
@@ -203,10 +201,8 @@ def open_replay(replay_file):
             team = 'Red'
         if i['m_teamId'] == 0:
             team = 'Blue'
-        player[player_number]['team'] = team
-        player[player_number]['playerName'] = i['m_name']
-        nameList.append(i['m_name'].decode())
-        
+        players[player_number]['team'] = team
+        players[player_number]['playerName'] = i['m_name'].decode()
         player_number += 1
 
     for i in protocol.decode_replay_message_events(messageevents):  
@@ -218,13 +214,12 @@ def open_replay(replay_file):
         if i ['_event'] == 'NNet.Game.SPingMessage':
             player_number = i['_userid']['m_userId'] - 1
             time = looptime(i['_gameloop'])
-            player[player_number]['Pings'].append(time)
+            players[player_number]['Pings'].append(time)
     stats = statistics.copy()
     excludeFromStats = ['TeamWinsDiablo','TeamWinsFemale', 'TeamWinsMale', 'TeamWinsStarCraft', 'TeamWinsWarcraft','WinsWarrior', 'WinsAssassin', 'WinsSupport','WinsSpecialist','WinsStarCraft', 'WinsDiablo', 'WinsWarcraft', 'WinsMale', 'WinsFemale', 'PlaysStarCraft', 'PlaysDiablo', 'PlaysOverwatch', 'PlaysWarCraft', 'PlaysWarrior', 'PlaysAssassin', 'PlaysSupport', 'PlaysSpecialist', 'PlaysMale', 'PlaysFemale', 'Tier1Talent', 'Tier2Talent', 'Tier3Talent', 'Tier4Talent', 'Tier5Talent', 'Tier6Talent', 'Tier7Talent', 'TeamLevel', 'LessThan4Deaths', 'LessThan3TownStructuresLost', 'Level', 'MetaExperience', 'TeamTakedowns', 'Role', 'EndOfMatchAwardGivenToNonwinner', 'GameScore', 'LunarNewYearSuccesfulArtifactTurnIns', 'LunarNewYearEventCompleted', 'StarcraftDailyEventCompleted', 'StarcraftPiecesCollected', 'LunarNewYearRoosterEventCompleted', 'PachimariMania', 'TouchByBlightPlague', 'EscapesPerformed', 'VengeancesPerformed', 'TeamfightEscapesPerformed', 'OutnumberedDeaths', 'EndOfMatchAwardMVPBoolean', 'EndOfMatchAwardHighestKillStreakBoolean', 'EndOfMatchAwardMostVengeancesPerformedBoolean', 'EndOfMatchAwardMostDaredevilEscapesBoolean', 'EndOfMatchAwardMostEscapesBoolean', 'EndOfMatchAwardMostXPContributionBoolean', 'EndOfMatchAwardMostHeroDamageDoneBoolean', 'EndOfMatchAwardMostKillsBoolean', 'EndOfMatchAwardHatTrickBoolean', 'EndOfMatchAwardClutchHealerBoolean', 'EndOfMatchAwardMostProtectionBoolean', 'EndOfMatchAward0DeathsBoolean', 'EndOfMatchAwardMostSiegeDamageDoneBoolean', 'EndOfMatchAwardMostDamageTakenBoolean', 'EndOfMatchAward0OutnumberedDeathsBoolean', 'EndOfMatchAwardMostHealingBoolean', 'EndOfMatchAwardMostStunsBoolean', 'EndOfMatchAwardMostRootsBoolean', 'EndOfMatchAwardMostSilencesBoolean', 'EndOfMatchAwardMostMercCampsCapturedBoolean', 'EndOfMatchAwardMostTeamfightDamageTakenBoolean', 'EndOfMatchAwardMostTeamfightHealingDoneBoolean', 'EndOfMatchAwardMostTeamfightHeroDamageDoneBoolean', 'EndOfMatchAwardMostDamageToMinionsBoolean', 'EndOfMatchAwardMapSpecificBoolean', 'EndOfMatchAwardMostDragonShrinesCapturedBoolean', 'EndOfMatchAwardMostTimePushingBoolean', 'EndOfMatchAwardMostTimeOnPointBoolean', 'EndOfMatchAwardMostInterruptedCageUnlocksBoolean', 'EndOfMatchAwardMostSeedsCollectedBoolean', 'EndOfMatchAwardMostDamageToPlantsBoolean', 'EndOfMatchAwardMostCurseDamageDoneBoolean', 'EndOfMatchAwardMostCoinsPaidBoolean', 'EndOfMatchAwardMostImmortalDamageBoolean', 'EndOfMatchAwardMostDamageDoneToZergBoolean', 'EndOfMatchAwardMostTimeInTempleBoolean', 'EndOfMatchAwardMostGemsTurnedInBoolean', 'EndOfMatchAwardMostSkullsCollectedBoolean', 'EndOfMatchAwardMostAltarDamageDone', 'EndOfMatchAwardMostNukeDamageDoneBoolean']
     for i in excludeFromStats:
         stats.pop(i, None)
-    print(player)
-    return [chatHistory, player, teamBlue, teamRed, stats, statistics, nameList, heroList, mapName, gameVersion, gameTime, gameLength, gameMode]
+    return [chatHistory, players, stats, statistics, gameDetails]
 
 
 
@@ -233,26 +228,15 @@ def replay_page():
     if request.method == 'POST':
         replay = request.files['file']
         returnData = open_replay(replay)
-        #try:
-        #    [global_chatHistory, global_player, global_teamBlue, global_teamRed, global_stats, global_statistics, global_nameList, global_heroList, global_mapName] = open_replay(replay)
-        #except:
-        #    cssURL = url_for('static', filename='home.css')
-        #    home_template = env.get_template('home.html')
-        #    print("error")
-        #    return home_template.render(cssURL=cssURL)
         
         chatlog = ""
         for i in returnData[0]:
-            chatlog += "{}({}:{}){}: {}".format("\n", format(int(i[0]//60), '02d'), format( int(i[0]%60), '02d'), player[int(i[1])]['playerName'].decode(), i[2].decode()) 
+            chatlog += "{}({}:{}){}: {}".format("\n", format(int(i[0]//60), '02d'), format( int(i[0]%60), '02d'), players[int(i[1])]['playerName'], i[2].decode()) 
 
-        talents = dict()
-        for i in returnData[1]:
-            talents[i['playerName'].decode()] = [i['partyPanelPortrait'], i['heroName'], "https://min.hyeok.org/SILVER/#/" + i['heroLink'] + '/' + i['talent'], i['talentIcon'], i['team']]
-        
         replay_template = env.get_template('replay.html')
         cssURL = url_for('static', filename='replay.css')
         jsURL = url_for('static', filename='replay.js')
-        return replay_template.render(cssURL=cssURL, jsURL=jsURL, chatlog=chatlog, talents=talents, stats=returnData[4], statistics=returnData[5], nameList=returnData[6], heroList=returnData[7], mapName=returnData[8], gameVersion=returnData[9], gameTime=returnData[10], gameLength=returnData[11], gameMode=returnData[12], chartTitle=chartTitle, chartLink=chartLink, chartLinkID=chartLinkID)
+        return replay_template.render(cssURL=cssURL, jsURL=jsURL, chatlog=chatlog, players=returnData[1], stats=returnData[2], statistics=returnData[3], gameDetails=returnData[4], chartTitle=chartTitle, chartLink=chartLink, chartLinkID=chartLinkID)
         
     else:
         cssURL = url_for('static', filename='home.css')
