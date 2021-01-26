5e7f43598622b7cc50fdc3d08f4a7fe97d7107e5
Minhyeok Seo <smh2153@gmail.com>
2021-01-26 19:17:21 +0900

    added notes

--- main.py
+++ main.py
@@ -9,31 +9,41 @@ env = Environment(
     loader=FileSystemLoader("templates"),
     autoescape=select_autoescape(["html", "xml"])
 )
-
+#used for making talent links
 talent_sort = ["80", "20", "0g", "04", "01"]
 talent_tier = [0, 1, 4, 7, 10, 13, 16, 20]
-chart_maximum = [0, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2500, 5000, 7500, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 125000, 150000, 175000, 200000, 225000, 250000, 275000, 300000, 350000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 2000000, 3000000, 5000000, 7000000, 10000000, 100000000, 1000000000, 10000000000, 100000000000, 1000000000000]
+#used for checking what mode the game was played in
 game_mode_dict = {-1: "Custom", 50001: "Quick Match", 50021: "Versus AI", 50031: "Brawl", 50041: "Practice", 50051: "Unranked Draft", 50061: "Hero League", 50071: "Team League", 50101:"ARAM"}
-
-
-new_chart_link = ["ExperienceContribution", "SiegeDamage", "HeroDamage", "TimeSpentDead", "DamageTaken", "TeamfightDamageTaken", "TeamfightHeroDamage", "MinionKills"]
-new_chart_title = ["EXP Contribution", "Siege Damage", "Hero Damage", "Time Spent Dead", "Damage Taken", "Teamfight Damage Taken", "Teamfight Damage Dealt", "Minion Kills"]
+#used for giving a ceiling for charts
+chart_maximum = [0, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2500, 5000, 7500, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 125000, 150000, 175000, 200000, 225000, 250000, 275000, 300000, 350000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 2000000, 3000000, 5000000, 7000000, 10000000, 100000000, 1000000000, 10000000000, 100000000000, 1000000000000]
+#used for giving reference to charts
 chart_link = ["Takedowns", "Deaths", "SoloKill", "Assists", "ExperienceContribution", "Healing", "SiegeDamage", "StructureDamage", "MinionDamage", "HeroDamage", "SelfHealing", "TimeSpentDead", "TimeCCdEnemyHeroes", "DamageTaken", "TimeSilencingEnemyHeroes", "TimeStunningEnemyHeroes", "TeamfightHealingDone", "TeamfightDamageTaken", "TeamfightHeroDamage", "PhysicalDamage", "SpellDamage", "MinionKills", "RegenGlobes"]
 chart_title = ["Kill Participation", "Deaths", "Kills", "Assists", "Experience Contribution", "Healing", "Siege Damage", "Structure Damage", "Minion Damage", "Hero Damage", "Self Healing", "Time Spent Dead", "Time CCing Enemy Heroes", "Damage Taken", "Time Silencing Enemy Heroes", "Time Stunning Enemy Heroes", "Teamfight Healing Done", "Teamfight Damage Taken", "Teamfight Hero Damage", "Physical Damage", "Spell Damage", "Minion Kills", "Regen Globes"]
-
-global chat_history, team_blue, team_red, players
+#announcing global variables
+#team_blue, team_red will be used for timeline
+global team_blue, team_red, players
+#players 0-4 is in team blue, 5-9 is in team red(i.e order and chaos)
 players = list(dict() for i in range(0, 10))
+#version will always be the base build for the most recent version
 version = "83716"
+#default language will be US english 
 language = "enus"
+#opening json file parsed from the game
 with open("./json/herodata_{}_{}.json".format(version, language), encoding="utf-8") as json_file:
     hero_data = json.load(json_file)
 
 
-
+#gametime will be given in integer t, after going through this function, it will represent time in seconds
 def looptime(t):
     return (t - 610) / 16
 
+#this is considered to be the main function of this code
 def open_replay(replay_file):
+    #every players will have its corresponding dictionary, filled with info
+    #regen_globes_time, pings, spray, voiceline, death will record the time it has occurred
+    #player_kill will be in the form of (time, number of victim)
+    #talent will be in the form of a string, with length 14
+    #talent_icon will be in the form of a list of length 7, each element representing one talent in order. default value will lead to a icon of a unselected monk level 1 talent(i.e a question mark icon)
     for i in players:
         i["regen_globes_time"] = list()
         i["pings"] = list()
@@ -43,21 +53,22 @@ def open_replay(replay_file):
         i["death"] = list()
         i["talent"] = ""
         i["talent_icon"] = ["storm_ui_icon_monk_trait1.png"] * 7
-    chat_history = list()
+    #team_blue, team_red will each record the time of a event happening
+    #level_up will be the time of when the level up happened
+    #camp_capture will be in the form of (time, captured camp)
     team_blue = dict()
-    team_red = dict()
+    team_red = dict()   
     team_blue["level_up"] = list(0 for i in range(0, 2))
     team_red["level_up"] = list(0 for i in range(0, 2))
     team_blue["camp_capture"] = list()
     team_red["camp_capture"] = list()
-
+    chatlog = ""
 
     
-    #filename = tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("replay files","*.StormReplay"),("all files","*.*")))
-    #just disabling choosing file while in the making
-    #filename = "C:/Users/smh21/Desktop/silvercity/2020-06-26 18.03.00 브락시스 전초기지.StormReplay"
+    #start by reading the header(it can be done with any protocol)
+    #from the header open the corresponding version of protocol
+    #reference for mpyq files https://github.com/eagleflo/mpyq. although it is not necessary in this occasion
     archive = mpyq.MPQArchive(replay_file)
-    # Read the protocol header, this can be read with any protocol
     header = latest().decode_replay_header(archive.header["user_data_header"]["content"])
     baseBuild = header["m_version"]["m_baseBuild"]
     try:
@@ -65,45 +76,51 @@ def open_replay(replay_file):
     except:
         print >> sys.stderr, "Unsupported base build: %d" % baseBuild
         sys.exit(1)
-
+    #each of these will be either a dictionary or a list with data in it
     details = protocol.decode_replay_details(archive.read_file("replay.details"))    
     init_data = protocol.decode_replay_initdata(archive.read_file("replay.initdata"))
     game_events = archive.read_file("replay.game.events")
     message_events = archive.read_file("replay.message.events")
     attribute_events = protocol.decode_replay_attributes_events(archive.read_file("replay.attributes.events"))
-
+    
+    #game_details will be a dictionary with certain details of the game
     game_details = dict()
+    #the name of the map the game was played on
     game_details["map_name"] = details["m_title"].decode()
+    #the version which the game was played on
     game_details["game_version"] = "{}.{}.{}.{}".format(header["m_version"]["m_major"], header["m_version"]["m_minor"], header["m_version"]["m_revision"], header["m_version"]["m_baseBuild"])
+    #the regional time this game was played on
     game_details["game_time"] = datetime.fromtimestamp((details["m_timeUTC"] + details["m_timeLocalOffset"]) // 10000000 - 11644506000)
+    #the length of the game
     game_details["game_length"] = "{0:02d}:{1:02d}".format(int(header["m_elapsedGameLoops"] / 16) // 60, int((header["m_elapsedGameLoops"] / 16) % 60))
+    #the winner of the game(assuming there are no ties)
+    game_details["winner"] = ["Blue", "Red"][details["m_playerList"][0]["m_result"] - 1]
+    #the game_mode this game is played on(default set to Unknown to avoid error)
     game_details["game_mode"] = "Unknown"
     if init_data["m_syncLobbyState"]["m_gameDescription"]["m_gameOptions"]["m_ammId"] in game_mode_dict:
         game_details["game_mode"] = game_mode_dict[init_data["m_syncLobbyState"]["m_gameDescription"]["m_gameOptions"]["m_ammId"]]
 
+    #will be going through tracker events
     if hasattr(protocol, "decode_replay_tracker_events"):
-        contents = archive.read_file("replay.tracker.events")
-
-        for event in protocol.decode_replay_tracker_events(contents):   
+        contents = protocol.decode_replay_tracker_events(archive.read_file("replay.tracker.events"))
+        for event in contents:   
+            #"NNet.Replay.Tracker.SScoreResultEvent" always happens at the end. I assume these stats are normally used to determine awards
             if event["_event"] == "NNet.Replay.Tracker.SScoreResultEvent":
                 statistics = dict()
                 statistics_maximum = dict()
-                
                 for i in event["m_instanceList"]:
                     values = list()
                     for j in i["m_values"][0:10]:
                         values.append(j[0]["m_value"])
                     statistics[i["m_name"].decode()] = values
+                    #this part is to determine the ceiling to use for charts
                     for j in range(0, 55):
                         if max(values) < chart_maximum[j]:
                             statistics_maximum[i["m_name"].decode()] = chart_maximum[j]
                             break
-
-
-
-
+            #checking for notable events
             if "m_eventName" in event:
-                #TimeSpentDead
+                #TimeSpentDead. Since this automatically adds time at the point of death, it may not correctly represent the actual time dead.
                 if event["m_eventName"].decode() == "EndOfGameTimeSpentDead":
                     time_spent_dead = event["m_fixedData"][0]["m_value"] / 4096
                     player_number = event["m_intData"][0]["m_value"] - 1
@@ -118,21 +135,18 @@ def open_replay(replay_file):
                         team_red["camp_capture"].append((time, event["m_stringData"][0]["m_value"]))
                     
 
-                #Level Up
+                #Level Up, will only be checking level up for user 0 and 5 since the level up time is the same for everyone else on the same team
                 if event["m_eventName"].decode() == "LevelUp":
                     time = looptime(event["_gameloop"])
                     if time > 0:
                         player_number = event["m_intData"][0]["m_value"] - 1
-                        if player_number < 5:
-                            if time != team_blue["level_up"][-1]:
-                                team_blue["level_up"].append(time)
-                        elif player_number <= 5:
-                            if time != team_red["level_up"][-1]:
-                                team_red["level_up"].append(time)
-                                
-                #Hero
-                
-                
+                        if player_number == 0:
+                            team_blue["level_up"].append(time)
+                        elif player_number == 5:
+                            team_red["level_up"].append(time)
+
+                #"EndOfGameTalentChoices" has multiple info inside, however only the hero names and talent choices will be used.
+                #Hero 
                 if event["m_eventName"].decode() == "EndOfGameTalentChoices":
                     player_number = event["m_intData"][0]["m_value"] - 1
                     j = 1
@@ -154,7 +168,6 @@ def open_replay(replay_file):
                             players[player_number]["hero_link"] = players[player_number]["Hero"]
                             players[player_number]["party_panel_portrait"] = hero_data[players[player_number]["hero_link"]]["portraits"]["partyPanel"]
                             players[player_number]["minimap_portrait"] = hero_data[players[player_number]["hero_link"]]["portraits"]["minimap"]
-                            
                 #Talents
                         elif i["m_key"].decode() != "Win/Loss" and i["m_key"] != "Map":
                             for which_talent in hero_data[players[player_number]["Hero"]]["talents"]["level{}".format(talent_tier[j])]:
@@ -190,7 +203,7 @@ def open_replay(replay_file):
                     player_number = event["m_intData"][0]["m_value"] - 1
                     players[player_number]["voiceline"].append(time)
 
-                #Regen Globe Pickup
+                #Regen Globe Pickup, this might not function properly for heroes such as TLV or Rexxar
                 if event["m_eventName"].decode() == "RegenGlobePickedUp":
                     time = looptime(event["_gameloop"])
                     player_number = event["m_intData"][0]["m_value"] - 1
@@ -198,37 +211,33 @@ def open_replay(replay_file):
                     
                 
 
-
+    #This part is needed for the talent link to function as intended even in cases where the game ends pre 20
     for i in players:
         i["talent"] = i["talent"].ljust(14, "0")
-    # Hero name, Player name, Team
+        
+    #Player name, Team
     player_number = 0
     for i in details["m_playerList"]:
-        team = "other"   
-        if i["m_teamId"] == 0:
-            team = "blue"
-        if i["m_teamId"] == 1:
-            team = "red"
-
-        players[player_number]["team"] = team
+        players[player_number]["team"] = ["blue", "red"][i["m_teamId"]]
         players[player_number]["player_name"] = i["m_name"].decode()
         player_number += 1
-
+    
     for i in protocol.decode_replay_message_events(message_events):  
+    #Chat Messages
         if i["_event"] == "NNet.Game.SChatMessage":
-            time = looptime(i["_gameloop"])
-            player_number = i["_userid"]["m_userId"]
-            words = i["m_string"]
-            chat_history.append((time, player_number, words)) 
+            chatlog += "{}({}:{}){}: {}".format("\n", format(int(looptime(i["_gameloop"])//60), "02d"), format( int(looptime(i["_gameloop"])%60), "02d"), players[i["_userid"]["m_userId"]]["player_name"], i["m_string"].decode()) 
+    #Ping Messages
         if i ["_event"] == "NNet.Game.SPingMessage":
-            player_number = i["_userid"]["m_userId"] - 1
+            player_number = i["_userid"]["m_userId"]
             time = looptime(i["_gameloop"])
             players[player_number]["pings"].append(time)
+    #This will soon be changed
     stats = statistics.copy()
     exclude_from_stats = ["TeamWinsDiablo","TeamWinsFemale", "TeamWinsMale", "TeamWinsStarCraft", "TeamWinsWarcraft","WinsWarrior", "WinsAssassin", "WinsSupport","WinsSpecialist","WinsStarCraft", "WinsDiablo", "WinsWarcraft", "WinsMale", "WinsFemale", "PlaysStarCraft", "PlaysDiablo", "PlaysOverwatch", "PlaysWarCraft", "PlaysWarrior", "PlaysAssassin", "PlaysSupport", "PlaysSpecialist", "PlaysMale", "PlaysFemale", "Tier1Talent", "Tier2Talent", "Tier3Talent", "Tier4Talent", "Tier5Talent", "Tier6Talent", "Tier7Talent", "TeamLevel", "LessThan4Deaths", "LessThan3TownStructuresLost", "Level", "MetaExperience", "TeamTakedowns", "Role", "EndOfMatchAwardGivenToNonwinner", "GameScore", "LunarNewYearSuccesfulArtifactTurnIns", "LunarNewYearEventCompleted", "StarcraftDailyEventCompleted", "StarcraftPiecesCollected", "LunarNewYearRoosterEventCompleted", "PachimariMania", "TouchByBlightPlague", "EscapesPerformed", "VengeancesPerformed", "TeamfightEscapesPerformed", "OutnumberedDeaths", "EndOfMatchAwardMVPBoolean", "EndOfMatchAwardHighestKillStreakBoolean", "EndOfMatchAwardMostVengeancesPerformedBoolean", "EndOfMatchAwardMostDaredevilEscapesBoolean", "EndOfMatchAwardMostEscapesBoolean", "EndOfMatchAwardMostXPContributionBoolean", "EndOfMatchAwardMostHeroDamageDoneBoolean", "EndOfMatchAwardMostKillsBoolean", "EndOfMatchAwardHatTrickBoolean", "EndOfMatchAwardClutchHealerBoolean", "EndOfMatchAwardMostProtectionBoolean", "EndOfMatchAward0DeathsBoolean", "EndOfMatchAwardMostSiegeDamageDoneBoolean", "EndOfMatchAwardMostDamageTakenBoolean", "EndOfMatchAward0OutnumberedDeathsBoolean", "EndOfMatchAwardMostHealingBoolean", "EndOfMatchAwardMostStunsBoolean", "EndOfMatchAwardMostRootsBoolean", "EndOfMatchAwardMostSilencesBoolean", "EndOfMatchAwardMostMercCampsCapturedBoolean", "EndOfMatchAwardMostTeamfightDamageTakenBoolean", "EndOfMatchAwardMostTeamfightHealingDoneBoolean", "EndOfMatchAwardMostTeamfightHeroDamageDoneBoolean", "EndOfMatchAwardMostDamageToMinionsBoolean", "EndOfMatchAwardMapSpecificBoolean", "EndOfMatchAwardMostDragonShrinesCapturedBoolean", "EndOfMatchAwardMostTimePushingBoolean", "EndOfMatchAwardMostTimeOnPointBoolean", "EndOfMatchAwardMostInterruptedCageUnlocksBoolean", "EndOfMatchAwardMostSeedsCollectedBoolean", "EndOfMatchAwardMostDamageToPlantsBoolean", "EndOfMatchAwardMostCurseDamageDoneBoolean", "EndOfMatchAwardMostCoinsPaidBoolean", "EndOfMatchAwardMostImmortalDamageBoolean", "EndOfMatchAwardMostDamageDoneToZergBoolean", "EndOfMatchAwardMostTimeInTempleBoolean", "EndOfMatchAwardMostGemsTurnedInBoolean", "EndOfMatchAwardMostSkullsCollectedBoolean", "EndOfMatchAwardMostAltarDamageDone", "EndOfMatchAwardMostNukeDamageDoneBoolean", "KilledTreasureGoblin", "TimeOnPoint", "CageUnlocksInterrupted", "GardenSeedsCollectedByPlayer", "PlaysNexus", "PlaysOverwatchOrNexus", "Multikill", "DamageDoneToZerg"]
     for i in exclude_from_stats:
         stats.pop(i, None)
-    return [chat_history, players, stats, statistics, statistics_maximum, game_details]
+    #-------------------------
+    return [chatlog, players, stats, statistics, statistics_maximum, game_details]
 
 
 
@@ -238,14 +247,11 @@ def replay_page():
         replay = request.files["file"]
         return_data = open_replay(replay)
         
-        chatlog = ""
-        for i in return_data[0]:
-            chatlog += "{}({}:{}){}: {}".format("\n", format(int(i[0]//60), "02d"), format( int(i[0]%60), "02d"), players[int(i[1])]["player_name"], i[2].decode()) 
-
+        
         replay_template = env.get_template("replay.html")
         css_URL = url_for("static", filename="replay.css")
         js_URL = url_for("static", filename="replay.js")
-        return replay_template.render(css_URL=css_URL, js_URL=js_URL, chatlog=chatlog, players=return_data[1], stats=return_data[2], statistics=return_data[3], statistics_maximum=return_data[4], game_details=return_data[5], chart_title=chart_title, chart_link=chart_link)
+        return replay_template.render(css_URL=css_URL, js_URL=js_URL, chatlog=return_data[0], players=return_data[1], stats=return_data[2], statistics=return_data[3], statistics_maximum=return_data[4], game_details=return_data[5], chart_title=chart_title, chart_link=chart_link)
         
     else:
         css_URL = url_for("static", filename="home.css")
